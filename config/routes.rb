a5be03b95bf91206e9364b24e821a776d3ecf0b7
Slađan Simic <SIM@SIMs-MacBook-Pro.local>
2017-04-25 19:23:21 +0200

    rails_apps_composer: add README files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'visitors#index'
+  resources :visitors, only: [:new, :create]
+  root to: 'visitors#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
