953d3adcf82f1c1a914c4249a57242feaf1fb1e6
Slađan Simic <SIM@SIMs-MacBook-Pro.local>
2017-04-25 19:23:27 +0200

    rails_apps_composer: extras

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   resources :visitors, only: [:new, :create]
   root to: 'visitors#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
